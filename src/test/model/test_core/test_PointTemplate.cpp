568dc01fb8c740698cc2fc6f741312a777d5d779
Giacomo Groppi <giamg01@gmail.com>
2023-07-15 10:27:55 +0200

    Add new tests for PointTemplate

--- src/test/model/test_core/test_PointTemplate.cpp
+++ src/test/model/test_core/test_PointTemplate.cpp
@@ -12,15 +12,37 @@ Q_OBJECT
 private slots:
     void rotate_distance_0();
     void rotate_distance_1_45();
+    void rotate_distance_1_m_45();
+
+    // distance 1 with cir = (1, 0) point (0, 0) 90°
+    void test1();
 };
+
+void test_PointTemplate::test1()
+{
+    PointTemplate<double> point  {0, 0};
+    PointTemplate<double> result {0, 1};
+    const auto actual = point.rotate({1, 0}, 90);
+    QVERIFY(WCommonScript::is_near(actual.x(), result.x(), 0.));
+    QVERIFY(WCommonScript::is_near(actual.y(), result.y(), 0.));
+}
 
 void test_PointTemplate::rotate_distance_1_45()
 {
-    PointTemplate<double> point {
-            0, 1
-    };
-    PointTemplate<double> result {1, 0};
-    QCOMPARE(point.rotate({0, 0}, 90), result);
+    PointTemplate<double> point  {0, 1};
+    PointTemplate<double> result {-1, 0};
+    const auto actual = point.rotate({0, 0}, 90);
+    QVERIFY(WCommonScript::is_near(actual.x(), result.x(), 0.));
+    QVERIFY(WCommonScript::is_near(actual.y(), result.y(), WCommonScript::Power(10, -15)));
+}
+
+void test_PointTemplate::rotate_distance_1_m_45()
+{
+    PointTemplate<double> point { 0, 1 };
+    PointTemplate<double> result { 1, 0 };
+    const auto actual = point.rotate({0, 0}, -90);
+    QVERIFY(WCommonScript::is_near(actual.x(), result.x(), 0.));
+    QVERIFY(WCommonScript::is_near(actual.y(), result.y(), WCommonScript::Power(10, -15)));
 }
 
 void test_PointTemplate::rotate_distance_0()
@@ -31,7 +53,6 @@ void test_PointTemplate::rotate_distance_0()
     PointTemplate<double> result {0, 0};
     QCOMPARE(point.rotate({0, 0}, 45), result);
 }
-
 
 QTEST_GUILESS_MAIN(test_PointTemplate)
 
